596ac7b25f910f5ffc760b422192e1ce19e6eec7
mustafaertabak <mustafa.ert@hotmail.com>
2018-07-05 02:29:54 +0300

    introduction içeriği girliyor...

--- js/dropmenu.js
+++ js/dropmenu.js
@@ -1,12 +1,20 @@
 function dropmenu() {
-    document.getElementById("menucon").classList.add("show");
-    document.getElementById("menucon-giris").classList.remove("show");
     document.getElementById("kurulum").classList.add("show");
+    document.getElementById("kurulum-side").classList.add("show");
+    document.getElementById("giris").classList.remove("show");
+    document.getElementById("giris-side").classList.remove("show");
+    document.getElementById("giris").classList.add("hidden");
+    document.getElementById("giris-side").classList.add("hidden");
+
 }
 
 function dropmenu_giris() {
-  document.getElementById("menucon-giris").classList.add("show");
-  document.getElementById("menucon").classList.remove("show");
+  document.getElementById("giris").classList.add("show");
+  document.getElementById("giris-side").classList.add("show");
+  document.getElementById("kurulum").classList.remove("show");
+  document.getElementById("kurulum").classList.add("hidden");
+  document.getElementById("kurulum-side").classList.remove("show");
+  document.getElementById("kurulum-side").classList.add("hidden");
 }
 
 
